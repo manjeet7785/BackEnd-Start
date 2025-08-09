5ef72ab6d0d8204a92e04c23b7fcb4f7b9837e1f
manjeet7785 <manjeetmaurya7785@gmail.com>
2025-08-09 23:55:02 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,12 @@
+
+console.log("Hello Sir How are You ?");
+
+console.log("Namste");
+
+
+// function getFun() {
+//   console.log("How are You??");
+// }
+// getFun();
+
+// export default getFun; 
---
+++ index.js
@@ -0,0 +1,14 @@
+const sum = require('./first.js')
+// import './first.js';
+
+
+function sum(a, b) {
+  console.log(a + b);
+
+}
+
+console.log("HHello ji");
+sum(5, 3);
+// const getFun = require('./first');
+
+// getFun();23
